27357ac7eb65de174b4ef02c407fafc30730c65f
Maty910 <matychacong@gmail.com>
2025-06-02 20:32:57 -0300

    fix: pictures path

--- src/PicturesSection.jsx
+++ src/PicturesSection.jsx
@@ -5,23 +5,23 @@ import { Footer } from './Footer'
 export function PicturesSection () {
   return (
     <>
-    <Header />
-    <section className="pm-pictures-section">
-      <h2 className='pm-subtitle'>Pictures:</h2>
-      <div className="pm-pictures-container">
-        <img src="./pictures/1.jpg" alt="Image description" />
-        <img src="./pictures/2.jpg" alt="Image description" />
-        <img src="./pictures/3.jpg" alt="Image description" />
-        <img src="./pictures/4.jpg" alt="Image description" />
-        <img src="./pictures/5.jpg" alt="Image description" />
-        <img src="./pictures/6.jpg" alt="Image description" />
-        <img src="./pictures/7.jpg" alt="Image description" />
-        <img src="./pictures/8.jpg" alt="Image description" />
-        <img src="./pictures/9.jpg" alt="Image description" />
-        <img src="./pictures/10.jpg" alt="Image description" />
-      </div>
-    </section>
-    <Footer />
+      <Header />
+      <section className="pm-pictures-section">
+        <h2 className='pm-subtitle'>Pictures:</h2>
+        <div className="pm-pictures-container">
+          <img src="/pictures/1.jpg" alt="Image description" />
+          <img src="/pictures/2.jpg" alt="Image description" />
+          <img src="/pictures/3.jpg" alt="Image description" />
+          <img src="/pictures/4.jpg" alt="Image description" />
+          <img src="/pictures/5.jpg" alt="Image description" />
+          <img src="/pictures/6.jpg" alt="Image description" />
+          <img src="/pictures/7.jpg" alt="Image description" />
+          <img src="/pictures/8.jpg" alt="Image description" />
+          <img src="/pictures/9.jpg" alt="Image description" />
+          <img src="/pictures/10.jpg" alt="Image description" />
+        </div>
+      </section>
+      <Footer />
     </>
   )
 }
