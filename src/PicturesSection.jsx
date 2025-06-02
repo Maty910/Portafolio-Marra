3e41931dfb20698492f37c3abbaf314d28a50244
Maty910 <matychacong@gmail.com>
2025-06-02 20:25:59 -0300

    .

--- src/PicturesSection.jsx
+++ src/PicturesSection.jsx
@@ -9,16 +9,16 @@ export function PicturesSection () {
     <section className="pm-pictures-section">
       <h2 className='pm-subtitle'>Pictures:</h2>
       <div className="pm-pictures-container">
-        <img src="public/pictures/1.jpg" alt="Image description" />
-        <img src="public/pictures/2.jpg" alt="Image description" />
-        <img src="public/pictures/3.jpg" alt="Image description" />
-        <img src="public/pictures/4.jpg" alt="Image description" />
-        <img src="public/pictures/5.jpg" alt="Image description" />
-        <img src="public/pictures/6.jpg" alt="Image description" />
-        <img src="public/pictures/7.jpg" alt="Image description" />
-        <img src="public/pictures/8.jpg" alt="Image description" />
-        <img src="public/pictures/9.jpg" alt="Image description" />
-        <img src="public/pictures/10.jpg" alt="Image description" />
+        <img src=".././public/pictures/1.jpg" alt="Image description" />
+        <img src=".././public/pictures/2.jpg" alt="Image description" />
+        <img src=".././public/pictures/3.jpg" alt="Image description" />
+        <img src=".././public/pictures/4.jpg" alt="Image description" />
+        <img src=".././public/pictures/5.jpg" alt="Image description" />
+        <img src=".././public/pictures/6.jpg" alt="Image description" />
+        <img src=".././public/pictures/7.jpg" alt="Image description" />
+        <img src=".././public/pictures/8.jpg" alt="Image description" />
+        <img src=".././public/pictures/9.jpg" alt="Image description" />
+        <img src=".././public/pictures/10.jpg" alt="Image description" />
       </div>
     </section>
     <Footer />
