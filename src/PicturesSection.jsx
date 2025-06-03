6b5c1e32f7a1d49654f535f026515301be1758cf
Maty910 <matychacong@gmail.com>
2025-06-02 21:24:57 -0300

    feat: modal from pictures section

--- src/PicturesSection.jsx
+++ src/PicturesSection.jsx
@@ -2,25 +2,37 @@ import './App.css'
 import { Header } from './Header.jsx' 
 import { Footer } from './Footer'
 
+import {useState} from 'react'
+
 export function PicturesSection () {
+  const [selectedPicture, setSelectedPicture] = useState(null)
+
+  const pictures = Array.from({ length: 10} , (_, i) => `/pictures/${i + 1}.jpg`)
+
   return (
     <>
       <Header />
-      <section className="pm-pictures-section">
-        <h2 className='pm-subtitle'>Pictures:</h2>
-        <div className="pm-pictures-container">
-          <img src="/pictures/1.jpg" alt="Image description" />
-          <img src="/pictures/2.jpg" alt="Image description" />
-          <img src="/pictures/3.jpg" alt="Image description" />
-          <img src="/pictures/4.jpg" alt="Image description" />
-          <img src="/pictures/5.jpg" alt="Image description" />
-          <img src="/pictures/6.jpg" alt="Image description" />
-          <img src="/pictures/7.jpg" alt="Image description" />
-          <img src="/pictures/8.jpg" alt="Image description" />
-          <img src="/pictures/9.jpg" alt="Image description" />
-          <img src="/pictures/10.jpg" alt="Image description" />
-        </div>
-      </section>
+        <section className="pm-pictures-section">
+          <h2 className='pm-subtitle'>Pictures:</h2>
+          <div className="pm-pictures-container">
+            <p className='pm-pictures-text'>Click on the images to see them in full size.</p>
+            {pictures.map((src, i) => (
+              <img
+                key={i}
+                src={src}
+                alt={`Image ${i + 1}`}
+                className="pm-picture"
+                onClick={() => setSelectedPicture(src)} style={{ cursor: 'pointer' }} 
+              />
+            ))}
+          </div>
+        </section>
+        {selectedPicture && (
+          <div className="pm-modal" onClick={() => setSelectedPicture(null)}>
+            <span className='pm-close' onClick={() => setSelectedPicture(null)}>&times;</span>
+            <img className='pm-modal-content' src={selectedPicture} alt='Full view' />
+          </div>
+        )}
       <Footer />
     </>
   )
