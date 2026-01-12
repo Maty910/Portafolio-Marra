b25ff29123dfff090a32a421f405ae26df6e042e
Maty910 <matychacong@gmail.com>
2026-01-11 23:04:01 -0300

    fix: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import ProjectPage from './ProjectPage.jsx'
-import ProjectsSection from './ProjectsSection.jsx'
-import PicturesSection from './PicturesSection.jsx'
-import Contact from './Contact.jsx'
-import Extras from './Extras.jsx'
+// Componentes de Páginas
+import { Home } from './Home.jsx';
+import ProjectsSection from './ProjectsSection.jsx'; // La página de grilla
+import ProjectPage from './ProjectPage.jsx';         // La página de detalle
+import ImagesSection from './ImagesSection.jsx';
+import Extras from './Extras.jsx';
+import Contact from './Contact.jsx';
 
-import './index.css'
-import { Home } from './Home.jsx'
+// Estilos globales
+import './index.css';
 
-export function App () {
+export function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route 
-            path="/" 
-            element={<Home />} 
-          /> {/* Página principal */}
-          <Route
-            path="/projects/:projectName"
-            element={<ProjectPage />}
-          /> {/* Página de proyecto */}
-          <Route
-            path="/projects"
-            element={<ProjectsSection />}
-          /> {/* Sección de proyectos */}
-        <Route
-            path="/photos"
-            element={<PicturesSection />}
-          /> {/* Sección de fotografías */}
-        <Route path="/extras" element={<Extras />} 
-          /> {/* Sección de extras */}
-        <Route
-            path="/contact"
-            element={<Contact />}
-          /> {/* Sección de contacto */}
-        </Routes>
-      </Router>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Página principal */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Sección de proyectos (Grilla) */}
+        <Route path="/projects" element={<ProjectsSection />} />
+        
+        {/* Página de proyecto individual (Detalle) */}
+        <Route path="/projects/:projectName" element={<ProjectPage />} />
+        
+        {/* Sección de fotografías */}
+        <Route path="/images" element={<ImagesSection />} />
+        
+        {/* Sección de extras */}
+        <Route path="/extras" element={<Extras />} />
+        
+        {/* Sección de contacto */}
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
