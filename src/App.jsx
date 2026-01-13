bcd073cc9bfe7e4243c4d0c23cc2230d51f5b998
Maty910 <matychacong@gmail.com>
2026-01-12 21:25:18 -0300

    feat: add Language Context component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BrandProvider } from './BrandContext.jsx'; // <--- IMPORTANTE
+import { LanguageProvider } from './LanguageContext.jsx';
 
 // Componentes
 import { Home } from './Home.jsx';
@@ -13,17 +14,19 @@ import './index.css';
 
 export function App() {
   return (
-    <BrandProvider> {/* <--- Envolvemos la app aquí */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<ProjectsSection />} />
-          <Route path="/projects/:projectName" element={<ProjectPage />} />
-          <Route path="/images" element={<ImagesSection />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-    </BrandProvider>
+    <LanguageProvider>
+      <BrandProvider> {/* <--- Envolvemos la app aquí */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<ProjectsSection />} />
+            <Route path="/projects/:projectName" element={<ProjectPage />} />
+            <Route path="/images" element={<ImagesSection />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </BrandProvider>
+    </LanguageProvider>
   );
 }
